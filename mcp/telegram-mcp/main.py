02636011876000071b50aaee1c16b3b7bcba91ce
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2026-06-14 21:21:55 +0530

    initial setup for telegram

--- mcp/telegram-mcp/main.py
+++ mcp/telegram-mcp/main.py
@@ -1,5 +1,17 @@
-from app.server import mcp
-import app.tools
+# from app.server import mcp
+# import app.tools
 
-if __name__ == "__main__":
-    mcp.run()
+# if __name__ == "__main__":
+#     mcp.run()
+
+from config import TELEGRAM_TOKEN
+import telebot
+
+
+bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    
+    bot.send_message(message.chat.id, "Hi, Welcome to Telegram!")
+bot.polling()
