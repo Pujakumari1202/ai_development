b8094a5470a7b6b308a1d618a2d96d83701a084f
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2026-06-23 13:35:03 +0530

    new schema added

--- data.sql
+++ data.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict XMn46o89kkPvhUMbnjKDa7NBsQUhbTj9kBaCjDDgsAPRPw1yoxwQs8XkvRiSXgh
+\restrict IMuK3QFOuCCBaNRQfl7qAnVrd0crOWUucArgvFFzA1prS0A7R9aVWfSN9uyK8Gq
 
--- Dumped from database version 18.4 (Debian 18.4-1.pgdg13+1)
--- Dumped by pg_dump version 18.4 (Debian 18.4-1.pgdg13+1)
+-- Dumped from database version 17.10 (Debian 17.10-1.pgdg13+1)
+-- Dumped by pg_dump version 17.10 (Debian 17.10-1.pgdg13+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,31 +20,81 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.product VALUES ('QB001', 'Classic White Bread', 'Soft and freshly baked white sandwich bread', 45.00, 1);
-INSERT INTO public.product VALUES ('QB002', 'Whole Wheat Bread', 'Healthy whole wheat loaf rich in fiber', 55.00, 2);
-INSERT INTO public.product VALUES ('QB003', 'Multigrain Bread', 'Nutritious bread made with multiple grains and seeds', 65.00, 3);
-INSERT INTO public.product VALUES ('QB004', 'Milk Bread', 'Soft and fluffy milk bread loaf', 60.00, 4);
-INSERT INTO public.product VALUES ('QB005', 'Garlic Bread', 'Freshly baked garlic-flavored bread', 80.00, 5);
-INSERT INTO public.product VALUES ('QB006', 'Brown Bread', 'Low-fat brown bread suitable for daily consumption', 50.00, 6);
-INSERT INTO public.product VALUES ('QB007', 'Burger Buns', 'Pack of 6 soft burger buns', 70.00, 7);
-INSERT INTO public.product VALUES ('QB008', 'Hot Dog Buns', 'Pack of 6 freshly baked hot dog buns', 70.00, 8);
-INSERT INTO public.product VALUES ('QB009', 'Chocolate Bread', 'Sweet bread with chocolate filling', 90.00, 9);
-INSERT INTO public.product VALUES ('QB010', 'Fruit Bread', 'Bread loaf containing mixed dry fruits and raisins', 95.00, 10);
+INSERT INTO public.customers VALUES (1, 'Nishat', '7980827269');
 
 
 --
--- Name: product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Data for Name: suppliers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.product_id_seq', 10, true);
+INSERT INTO public.suppliers VALUES (1, 'Puja', '8583930417', '1 day');
+INSERT INTO public.suppliers VALUES (2, 'Farhan', '6290363971', '2 days');
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.orders VALUES (1, 1, 1);
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.products VALUES (1, 'QBake Bread', 1, 5.00);
+INSERT INTO public.products VALUES (2, 'Luisine Bread', 1, 5.25);
+INSERT INTO public.products VALUES (3, 'Luisine Bread', 2, 5.15);
+
+
+--
+-- Data for Name: order_lines; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.order_lines VALUES (1, 2, 1, 5.00, -0.25, 200);
+
+
+--
+-- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.customers_customer_id_seq', 1, false);
+
+
+--
+-- Name: order_lines_order_line_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.order_lines_order_line_id_seq', 1, false);
+
+
+--
+-- Name: orders_order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_order_id_seq', 1, false);
+
+
+--
+-- Name: products_product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.products_product_id_seq', 1, false);
+
+
+--
+-- Name: suppliers_supplier_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.suppliers_supplier_id_seq', 1, false);
 
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict XMn46o89kkPvhUMbnjKDa7NBsQUhbTj9kBaCjDDgsAPRPw1yoxwQs8XkvRiSXgh
+\unrestrict IMuK3QFOuCCBaNRQfl7qAnVrd0crOWUucArgvFFzA1prS0A7R9aVWfSN9uyK8Gq
 
